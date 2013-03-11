e54157ba0eaff5bd017907c3bb0ff5574a2efd93
Roman Heinrich <roman.heinrich@gmail.com>
2013-03-11 18:05:38 +0200

    tests

--- test/config_test.rb
+++ test/config_test.rb
@@ -1,23 +1,27 @@
 require './test/test_helper'
 
+
 describe "Riemann::Dash::Config" do
+    before do
+      @config = Riemann::Dash::Config.instance
+    end
+
+    after do
+      Riemann::Dash::Config.reset!
+    end
+
   it "is loaded" do
-    Riemann::Dash::Config.new("").wont_be_nil
+    @config.wont_be_nil
   end
 
   describe :load_config do
     it "will apply configs from specified file to sinatra app" do
-      config = Riemann::Dash::Config.new("test/fixtures/basic_config.rb")
-      config.load_config
+      @config.load_config("test/fixtures/config/basic_config.rb")
       Riemann::Dash::App.settings.settings_loaded.must_equal "yes"
     end
   end
 
   describe :setup_default_values do
-    before do
-      @config = Riemann::Dash::Config.new("")
-    end
-
     it "has controllers path" do
       @config.store[:controllers].first.must_match %r{/lib/riemann/dash/controller}
       @config.store[:controllers].class.must_equal Array
@@ -37,16 +41,36 @@ describe "Riemann::Dash::Config" do
 
   end
 
-  describe :load_controllers do
+  describe "applying to sinatra app" do
+    describe :load_controllers do
 
+    end
+
+    describe :setup_views do
+
+    end
+
+
+    describe :setup_public_dir do
+
+    end
   end
 
-  describe :setup_views do
+  describe "workspace config" do
+    describe :read_ws_config do
+      it "retuns hash for empty configs" do
+        @config.read_ws_config.must_equal "{}"
+      end
 
-  end
+      it "reads the file, if present" do
+        @config.load_config("test/fixtures/config/ws_config.rb").must_equal true
+        @config.store[:ws_config].must_equal "test/fixtures/ws_config/dummy_config.json"
+        @config.read_ws_config.must_equal %Q{{hey: \"ho\"}}
+      end
+    end
 
+    describe :update_ws_config do
 
-  describe :setup_public_dir do
-
+    end
   end
 end
