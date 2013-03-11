df84cd51c4ba8f7055f4ffe89a431ec334422ba2
Roman Heinrich <roman.heinrich@gmail.com>
2013-03-11 13:29:21 +0200

    rake test task

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,3 +1,11 @@
 #$:.unshift(File.join(File.dirname(__FILE__), 'lib'))
 #require 'rubygems'
 require "bundler/gem_tasks"
+
+task :default => :test
+require 'rake/testtask'
+Rake::TestTask.new do |t|
+  t.libs.push "lib"
+  t.test_files = FileList['test/**/*_test.rb']
+  t.verbose    = false
+end
