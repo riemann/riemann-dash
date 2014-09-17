52072b943e025c23b7416247059ceed521b65e0c
Marc Fournier <marc.fournier@camptocamp.com>
2014-09-17 10:02:46 +0200

    add example for :bind option

--- example/config.rb
+++ example/config.rb
@@ -1,5 +1,10 @@
+# Example configuration file for riemann-dash.
+
 # Serve HTTP traffic on this port
 set  :port, 4567
+
+# Answer queries sent to this IP address
+set  :bind, "0.0.0.0"
 
 riemann_base = '.'
 riemann_src = "#{riemann_base}/lib/riemann/dash"
